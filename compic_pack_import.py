6847e5a3cab4da996275589d368f6d1b86490123
Aethaeran <33813964+Aethaeran@users.noreply.github.com>
2021-08-29 20:29:21 -0700

    fix empty list declaration
    In Python, the equivalent of a bash "array" is called a "list". An empty list is defined with two square brackets like so.

--- compic_pack_import.py
+++ compic_pack_import.py
@@ -38,7 +38,7 @@ CWD = os.getcwd()
 pack_dir = os.path.join(CWD,"packs")  # Root directory where your unsorted comic packs reside
 proc_dir = os.path.join(CWD,"process")  # Directory to move files for post-processing in Mylar
 error_dir = os.path.join(CWD,"errors")  # Directory to store errored comic files
-filename_search = ()  # Create an array to store file name searches for CV search
+filename_search = []  # Create an list to store file name searches for CV search
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
 
 ###############################################################
